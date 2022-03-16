c9631c70c4ad71520b9432b8cec337f7ff2f674b
Shota Yamamoto <kuaiwanga@gamil.com>
2022-03-16 22:07:44 +0900

    search

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from posts import views
 urlpatterns = [
     path('category/<str:category>/', views.category, name='category'),
     path('tag/<str:tag>/', views.tag, name='tag'),
+    path('search/', views.search, name='search'),
     path('<slug:slug>/', views.detail, name="detail"), #/ないとエラー出る
     path('', views.index, name="index"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,48 @@
-from unicodedata import name
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Category, Post, Tag
+from django.db.models import Q
+from django.contrib import messages
+from functools import reduce
+from operator import and_
 
 # Create your views here.
 def index(request):
     posts = Post.objects.order_by('-created_at')
     return render(request, 'posts/index.html', {'posts': posts})
+
+def search(request):
+    posts = Post.objects.order_by('-created_at')
+    keyword = request.GET.get('keyword')
+
+    if keyword:
+        """ 除外リストを作成 """
+        exclusion_list = set([' ', '　'])
+        q_list = ''
+
+        for i in keyword:
+            """ 全角半角の空文字が含まれていたら無視 """
+            if i in exclusion_list:
+                pass
+            else:
+                q_list += i
+
+        query = reduce(
+                    and_, [Q(title__icontains=q) | Q(category__name__icontains=q) | Q(tag__name__icontains=q) for q in q_list]
+                )
+                # fieldなら階層ありでもいける
+
+        posts = posts.filter(query)
+        if posts:
+            messages.success(request, f'「{keyword}」の検索結果')
+            messages.success(request, f'{len(posts)}件の記事が見つかりました')
+        else:
+            messages.success(request, f'「{keyword}」の検索結果')
+            messages.success(request, f'記事が見つかりませんでした')
+
+        return render(request, 'posts/search.html', {'posts': posts})
+
+    return redirect('index') #keywordが空の場合はindexにリダイレクト
+
 
 def category(request, category):
     category = Category.objects.get(name=category)
