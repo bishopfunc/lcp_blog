048329af7b8d5259d232a8b85cba772c4f960e1b
Shota Yamamoto <kuaiwanga@gamil.com>
2022-07-27 23:56:57 +0900

    cloudinary

--- lcp_blog/settings.py
+++ lcp_blog/settings.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DEBUG = False
+DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -119,7 +119,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'alert alert-danger',
 }
 
-if not DEBUG:
+if  DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku #追加
     django_heroku.settings(locals()) #追加
