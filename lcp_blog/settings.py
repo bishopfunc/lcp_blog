8eb60d891ee74f1ec7e019e340a79229059333eb
Shota Yamamoto <kuaiwanga@gamil.com>
2022-09-04 19:59:10 +0900

    delpoy

--- lcp_blog/settings.py
+++ lcp_blog/settings.py
@@ -3,7 +3,7 @@ import os
 import cloudinary
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DEBUG = False
+DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
